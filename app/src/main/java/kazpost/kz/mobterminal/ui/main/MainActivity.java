80d8be4ae0a1a6b3fbc0a005a4a08082cb6999c1
Zhangali Bidaibekov <tomas.mapoter@gmail.com>
2017-07-11 16:40:54 +0600

    version name in mainactivity

--- app/src/main/java/kazpost/kz/mobterminal/ui/main/MainActivity.java
+++ app/src/main/java/kazpost/kz/mobterminal/ui/main/MainActivity.java
@@ -5,17 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import kazpost.kz.mobterminal.BuildConfig;
 import kazpost.kz.mobterminal.R;
 import kazpost.kz.mobterminal.ui.base.BaseActivity;
 import kazpost.kz.mobterminal.ui.closecell.CloseCellActivity;
 import kazpost.kz.mobterminal.ui.print.ChoosePrinterActivity;
-import kazpost.kz.mobterminal.ui.print.PrintActivity;
 import kazpost.kz.mobterminal.ui.scan.ScanActivity;
 
 public class MainActivity extends BaseActivity implements MainMvpView {
@@ -30,12 +31,17 @@ public class MainActivity extends BaseActivity implements MainMvpView {
     Button btnConfigPrinter;
     @BindView(R.id.btn_exit)
     Button btnExit;
+    @BindView(R.id.tv_version_name)
+    TextView tvVersionName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
+        tvVersionName.append(BuildConfig.VERSION_NAME);
 
         getActivityComponent().inject(this);
         mPresenter.onAttach(MainActivity.this);
--- app/src/main/java/kazpost/kz/mobterminal/ui/print/PrintActivity.java
+++ app/src/main/java/kazpost/kz/mobterminal/ui/print/PrintActivity.java
@@ -1,6 +1,7 @@
 package kazpost.kz.mobterminal.ui.print;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -215,6 +216,28 @@ public class PrintActivity extends BaseActivity {
 
                 break;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        showDialog();
+//        super.onBackPressed();
+    }
+
+    private void showDialog() {
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.dialog_exit_message);
+        //                .setTitle(R.string.dialog_title);
+
+        builder.setPositiveButton("Да", (dialog, which) -> super.onBackPressed());
+        builder.setNegativeButton("Нет", ((dialog, which) -> dialog.dismiss()));
+
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
